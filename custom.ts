c53744f79086358bdb8a8a078228067d6201e2e8
jasonc1025-333 <67285598+jasonc1025-333@users.noreply.github.com>
2025-02-09 07:33:26 -0800

    25-0209-0730 Switch Order of 'quest_Send...' to reverse order on front-end

--- custom.ts
+++ custom.ts
@@ -30,6 +30,7 @@
 //   ** https://www.rapidtables.com/web/color/RGB_Color.html
 //   ** https://www.indezine.com/products/powerpoint/learn/color/color-rgb.html
 //   ** https://htmlcolorcodes.com/
+// * jwc 25-0209-0730 Need to refresh web-browser to see changes in front-end (create/change coding blocks)
 
 
 // enum MyEnum {
@@ -298,6 +299,47 @@ namespace quest_Dashboard {
     }
 
 
+    //// jwc yy export function quest_Send_DataOfBot_ToXrayDashboardOfMb_Func(network_GroupChannelOfMe_Base0_IntIn: number, send_DataOfBot_ToXrayDashboardOfMb_On_Bool_In: boolean, debug_Show_In: quest_Debug_Show_Enum): void {
+
+    /**
+     * quest_Send_DataOfBot_ToXrayDashboardOfMb_Func
+     * @param send_DataOfBot_ToXrayDashboardOfMb_Message_String string
+     * @param debug_Show_In quest_Debug_Show_Enum
+     *
+     */
+    // '\\' = escape character to deactivate following special character
+    //% block="set quest\\_Send\\_DataOfBot\\_ToXrayDashboardOfMb\\_Func:|* send_DataOfBot_ToXrayDashboardOfMb_Message_String: $send_DataOfBot_ToXrayDashboardOfMb_Message_String|* debug_Show_In: $debug_Show_In"
+    //% weight=50 blockGap=8
+    //% inlineInputMode=external
+    export function quest_Send_DataOfBot_ToXrayDashboardOfMb_Func(send_DataOfBot_ToXrayDashboardOfMb_Message_String: string, debug_Show_In: quest_Debug_Show_Enum): void {
+        ///jwc y if(quest_Debug_Show_Enum)
+        ///jwc y basic.showIcon(IconNames.SmallHeart)
+
+        let network_Message_Str = send_DataOfBot_ToXrayDashboardOfMb_Message_String
+
+        //// jwc send data
+
+        if (true) {
+            quest_Note_3.quest_Show_String_For_Note_Small_Func(
+                "Not end w/'delimiter' or will create a fake key_value pair at receiving end"
+            )
+            quest_Note_3.quest_Show_String_For_Note_Big_Func(
+                "Network_Message Max Length or will be cut off"
+            )
+            if (network_Message_Str.length > network_Message_LENGTH_MAX_INT) {
+                basic.showString("ERROR: 25-0209-0410 Network Message > Max Len.")
+            }
+            radio.setGroup(network_GroupChannel_Of_XrayDashboardOfMb_BASE0_INT)
+            radio.sendString(network_Message_Str)
+            radio.setGroup(network_GroupChannelOfMe_Base0_Int_QuestGlobal)
+            if (debug_Show_In = quest_Debug_Show_Enum.Dashboard_OLED) {
+                quest_Dashboard.quest_Show_String_For_Oled_SmallFont_Func("* 40: network_GroupChannel_Of_XrayDashboardOfMb_BASE0_INT: " + network_GroupChannel_Of_XrayDashboardOfMb_BASE0_INT + " | network_Message_Str: " + network_Message_Str, 0, 3)
+                serial.writeLine("* 40: network_GroupChannel_Of_XrayDashboardOfMb_BASE0_INT: " + network_GroupChannel_Of_XrayDashboardOfMb_BASE0_INT + " | network_Message_Str: " + network_Message_Str)
+            }
+        }
+
+    }
+
     //// jwc oyy /**
     //// jwc oyy  * quest_Set_PowerMotorsViaBlueRedBlackPins_WithTimer_Fn
     //// jwc oyy  * @param portIdsIn quest_PortGroup_BlueRedBlack_PortIds_Enum
@@ -408,49 +450,6 @@ namespace quest_Dashboard {
         }
 
     }
-
-    //// jwc yy export function quest_Send_DataOfBot_ToXrayDashboardOfMb_Func(network_GroupChannelOfMe_Base0_IntIn: number, send_DataOfBot_ToXrayDashboardOfMb_On_Bool_In: boolean, debug_Show_In: quest_Debug_Show_Enum): void {
-
-    /**
-     * quest_Send_DataOfBot_ToXrayDashboardOfMb_Func
-     * @param send_DataOfBot_ToXrayDashboardOfMb_Message_String string
-     * @param debug_Show_In quest_Debug_Show_Enum
-     *
-     */
-    // '\\' = escape character to deactivate following special character
-    //% block="set quest\\_Send\\_DataOfBot\\_ToXrayDashboardOfMb\\_Func:|* send_DataOfBot_ToXrayDashboardOfMb_Message_String: $send_DataOfBot_ToXrayDashboardOfMb_Message_String|* debug_Show_In: $debug_Show_In"
-    //% weight=50 blockGap=8
-    //% inlineInputMode=external
-    export function quest_Send_DataOfBot_ToXrayDashboardOfMb_Func(send_DataOfBot_ToXrayDashboardOfMb_Message_String: string, debug_Show_In: quest_Debug_Show_Enum): void {
-        ///jwc y if(quest_Debug_Show_Enum)
-        ///jwc y basic.showIcon(IconNames.SmallHeart)
-
-        let network_Message_Str = send_DataOfBot_ToXrayDashboardOfMb_Message_String
-
-        //// jwc send data
-
-        if (true) {
-            quest_Note_3.quest_Show_String_For_Note_Small_Func(
-                "Not end w/'delimiter' or will create a fake key_value pair at receiving end"
-            )
-            quest_Note_3.quest_Show_String_For_Note_Big_Func(
-                "Network_Message Max Length or will be cut off"
-            )
-            if (network_Message_Str.length > network_Message_LENGTH_MAX_INT) {
-                basic.showString("ERROR: 25-0209-0410 Network Message > Max Len.")
-            }
-            radio.setGroup(network_GroupChannel_Of_XrayDashboardOfMb_BASE0_INT)
-            radio.sendString(network_Message_Str)
-            radio.setGroup(network_GroupChannelOfMe_Base0_Int_QuestGlobal)
-            if (debug_Show_In = quest_Debug_Show_Enum.Dashboard_OLED) {
-                quest_Dashboard.quest_Show_String_For_Oled_SmallFont_Func("* 40: network_GroupChannel_Of_XrayDashboardOfMb_BASE0_INT: " + network_GroupChannel_Of_XrayDashboardOfMb_BASE0_INT + " | network_Message_Str: " + network_Message_Str, 0, 3)
-                serial.writeLine("* 40: network_GroupChannel_Of_XrayDashboardOfMb_BASE0_INT: " + network_GroupChannel_Of_XrayDashboardOfMb_BASE0_INT + " | network_Message_Str: " + network_Message_Str)
-            }
-        }
-
-    }
-
-
 
 }
 
